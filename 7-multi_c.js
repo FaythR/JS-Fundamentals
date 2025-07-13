e1b3fe531d0e99e6121f9564ea305434f8e694f3
FaythR <faithngangarw@gmail.com>
2025-07-13 22:12:02 +0200

    Fix 7-multi_c.js to produce no output for negative numbers

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,11 @@
 const arg = process.argv[2];
-     const num = parseInt(arg);
-     let i = 0;
-     if (isNaN(num) || num <= 0) {
-         console.log("Missing number of occurrences");
-     }
-     while (i < num) {
-         console.log("C is fun");
-         i++;
-     }
+const num = parseInt(arg);
+let i = 0;
+if (isNaN(num)) {
+    console.log("Missing number of occurrences");
+} else if (num > 0) {
+    while (i < num) {
+        console.log("C is fun");
+        i++;
+    }
+}
